7368aa8e57ffd431b88b8d48f1da428036b60dec
mustabur <mustaburrahman2017@hotmail.com>
2021-03-06 15:24:02 +0000

    test created for getting staff memeber by id

--- spec/spec.js
+++ spec/spec.js
@@ -2,6 +2,7 @@ const app = require("../server");
 const request = require("supertest");
 const chai = require("chai");
 const { expect } = require("chai");
+const db = require("../db/db_connection");
 
 describe("/api", () => {
   it("GET 200: responds with an array of all staff members from from the database", () => {
@@ -19,6 +20,27 @@ describe("/api", () => {
             "registered_data"
           );
         });
+      });
+  });
+});
+
+describe("/api", () => {
+  it("GET 200: responds with details of selected staff member using staff_id", () => {
+    return request(app)
+      .get("/api/1")
+      .then((response) => {
+        JSON.parse(response.text).forEach((staff) => {
+          expect(staff).to.have.keys(
+            "id",
+            "first_name",
+            "last_name",
+            "department",
+            "email",
+            "contact_number",
+            "registered_data"
+          );
+        });
+        expect(JSON.parse(response.text).length).to.equal(1)
       });
   });
 });
