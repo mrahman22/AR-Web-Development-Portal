b77237533968bb1461036b08dd597ac9fe496e8e
mustabur <mustaburrahman2017@hotmail.com>
2021-03-07 13:11:31 +0000

    unit test created for post request

--- spec/spec.js
+++ spec/spec.js
@@ -40,7 +40,51 @@ describe("/api", () => {
             "registered_data"
           );
         });
-        expect(JSON.parse(response.text).length).to.equal(1)
+        expect(JSON.parse(response.text).length).to.equal(1);
+      });
+  });
+});
+
+describe("/api", () => {
+  it("GET 200: responds with details of selected staff member using staff_id", () => {
+    return request(app)
+      .get("/api/1")
+      .then((response) => {
+        JSON.parse(response.text).forEach((staff) => {
+          expect(staff).to.have.keys(
+            "id",
+            "first_name",
+            "last_name",
+            "department",
+            "email",
+            "contact_number",
+            "registered_data"
+          );
+        });
+        expect(JSON.parse(response.text).length).to.equal(1);
+      });
+  });
+});
+
+describe("/api", () => {
+  it("POST 201 - posts a new staff member", () => {
+    return request(app)
+      .post("/api")
+      .send({
+        first_name: "Lopee",
+        last_name: "Johnson",
+        department: "Sales",
+        email: "lopeejohnson@ar-webdevelopment.com",
+        contact_number: "07900112233",
+      })
+      .then((response) => {
+        expect(response.request._data).to.have.keys(
+          "first_name",
+          "last_name",
+          "department",
+          "email",
+          "contact_number"
+        );
       });
   });
 });
